7d4e9badbf16f3d4180c3b09ff3cd1c24a7b62c7
sahilthakur7 <sahilrd07@gmail.com>
2019-11-05 22:16:07 +0530

    changed difficulty from hex to binary checks

---
+++ average-work.js
@@ -0,0 +1,30 @@
+const Blockchain = require('./blockchain');
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock({data: 'initial data'});
+
+let prevTimestamp,nextTimestamp,nextBlock,timeDiff,average;
+
+const times = [];
+
+for(let i=0 ; i < 10000 ; i++){
+     prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
+
+    blockchain.addBlock({data: `Block ${i}`});
+
+    nextBlock = blockchain.chain[blockchain.chain.length - 1];
+    nextTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
+    timeDiff = nextTimestamp - prevTimestamp;
+
+    times.push(timeDiff);
+
+    average = times.reduce((total,num) => (total+num))/times.length;
+    console.log("time to mine block", timeDiff);
+    console.log("Difficulty", nextBlock.difficulty);
+    console.log("average", average);
+}
+
+
+
+
