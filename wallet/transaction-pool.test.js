27046633bd17cbdd42ba0cf8e8952c7164433eeb
sahilthakur7 <sahilrd07@gmail.com>
2020-01-07 23:38:32 +0530

    Added tests for existing test

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -3,11 +3,12 @@ const Transaction = require('./transaction');
 const Wallet = require('./index.js');
 
 describe('transaction pool',() => {
-    let transactionPool, transaction;
+    let transactionPool, transaction , senderWallet;
 
     beforeEach(() => {
         transactionPool = new TransactionPool();
-        transaction = new Transaction({senderWallet: new Wallet() , recipient: 'fake', amount: 50});
+        senderWallet = new Wallet();
+        transaction = new Transaction({senderWallet, recipient: 'fake', amount: 50});
     });
 
     describe('set transaction', () => {
@@ -16,5 +17,15 @@ describe('transaction pool',() => {
 
             expect(transactionPool.transactionMap[transaction.id]).toBe(transaction);
         });
+    });
+
+    describe('existing transaction', () => {
+        it ('returns an existing transaction given an input address', () => {
+            transactionPool.setTransaction(transaction);
+
+            expect(
+                transactionPool.existingTransaction({inputAddress: senderWallet.publicKey})
+            ).toBe(transaction);
+        })
     })
 });
