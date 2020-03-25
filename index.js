9d4522eb16568933e45f1f3cc106358203865019
sahilthakur7 <sahilrd07@gmail.com>
2020-03-25 17:49:14 +0530

    Started with frontend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const request = require("request");
+const path = require('path');
 const Blockchain = require("./blockchain");
 const bodyParser = require("body-parser");
 const PubSub = require("./app/pubsub");
@@ -24,6 +25,7 @@ let PEER_PORT;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get("/api/blocks", (req, res) => {
   res.json(blockchain.chain);
@@ -85,6 +87,10 @@ app.get("/api/wallet-info", (req, res) => {
     })
   });
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/dist/index.html'));
+})
 
 const syncWithRootState = () => {
   request(
