7091f15d57b1b414982646a33c7e552420cced5a
sahilthakur7 <sahilrd07@gmail.com>
2020-04-21 13:10:04 +0530

    Added babel and fetched wallet info

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    walletInfo: { address: 'fooxb6', balance: 3000 }
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/wallet-info").then(res => res.json()).then(json => {
+      this.setState({
+        walletInfo: json
+      })
+    });
+  }
+
   render() {
+    const { address, balance } = this.state.walletInfo;
     return (
       <div>
         Welcome to the chain
+    <div>Address: {address} , balance: {balance}</div>
       </div>
     )
   }
