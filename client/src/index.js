1201966511161706f13f905011bfde5989af33d1
sahilthakur7 <sahilrd07@gmail.com>
2020-05-26 10:30:45 +0530

    Transaction pool added

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { Router, Switch, Route } from 'react-router-dom';
 import App from './Components/App';
 import Blocks from './Components/Blocks';
 import ConductTransaction from './Components/ConductTransaction';
+import TransactionPool from './Components/TransactionPool';
 import history from './history';
 import './index.css';
 
@@ -13,6 +14,7 @@ render(
       <Route exact path="/" component={App} />
       <Route path="/blocks" component={Blocks} />
       <Route path="/conduct-transaction" component={ConductTransaction} />
+      <Route path="/transaction-pool" component={TransactionPool} />
     </Switch>
   </Router>,
   document.querySelector("#app"));
